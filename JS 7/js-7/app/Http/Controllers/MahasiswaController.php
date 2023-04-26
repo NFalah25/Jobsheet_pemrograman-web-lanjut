0c4702aed4ebdf3fd48506d2ef87b2e7f13587a5
= Nfalah25 <= falahnaufal1@gmail.com>
2023-04-26 22:54:37 +0700

    first commit

---
+++ JS 7/js-7/app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use App\Http\Requests\StoreMahasiswaRequest;
+use App\Http\Requests\UpdateMahasiswaRequest;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::all();
+        $posts = Mahasiswa::orderBy('Nim', 'desc')->paginate(6);
+        return view('mahasiswa.index', compact('mahasiswa'))->
+        with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+//        Melakukan Validasi Data
+        $request->validate([
+            'Nim'=>'required',
+            'nama'=>'required',
+            'kelas'=>'required',
+            'jurusan'=>'required',
+            'no_handphone'=>'required',
+        ]);
+
+        Mahasiswa::create($request->all());
+
+        return redirect()->route('mahasiswa.index')->with('success', 'Mahasiswa Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($Nim)
+    {
+        //Menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
+        $Mahasiswa = Mahasiswa::find($Nim);
+        return view('mahasiswa.detail', compact('Mahasiswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($nim)
+    {
+        //menampilkan detail data dengan menemukan/berdasarkan nim Mahasiswa
+        $Mahasiswa = Mahasiswa::find($nim);
+        return view('mahasiswa.edit', compact('Mahasiswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $nim)
+    {
+        $request->validate([
+            'Nim'=>'required',
+            'nama'=>'required',
+            'kelas'=>'required',
+            'jurusan'=>'required',
+            'no_handphone'=>'required',
+        ]);
+
+        Mahasiswa::find($nim)->update($request->all());
+
+        return redirect()->route('mahasiswa.index')
+            ->with('success', 'Mahasiswa Berhasil Diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($nim)
+    {
+        //fungsi eloquent untuk menghapus data
+        Mahasiswa::find($nim)->delete();
+        return redirect()->route('mahasiswa.index')
+            ->with('success', 'Mahasiswa Berhasil Dihapus');
+    }
+}
---
+++ JS 7/js-7/app/Models/Mahasiswa.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table = "mahasiswa";
+
+    public $timestamps= false;
+    protected $primaryKey='Nim';
+
+    protected $fillable=[
+        'Nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_handphone'
+        ];
+//    use HasFactory;
+}
