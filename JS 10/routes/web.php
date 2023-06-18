100fd7b34c3d32ea2e967fea211302888dc50814
= Nfalah25 <= falahnaufal1@gmail.com>
2023-06-18 21:07:42 +0700

    JS-10

--- JS 10/routes/web.php
+++ JS 10/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\articleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resources([
+    'article'=>articleController::class,
+]);
+Route::get('cetak_pdf',[articleController::class,'cetak_pdf']);
